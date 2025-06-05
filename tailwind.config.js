1d6fd42c6678a341635cd693114d6e323f5d7836
Satrya Wiguna <satrya@innoxcell.net>
2025-06-05 22:56:48 +0800

    GUS-001_Setup-environtment

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import { Config } from "tailwindcss";
+import { heroui } from "@heroui/react";
+
+const config: Config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
+
+export default config;
